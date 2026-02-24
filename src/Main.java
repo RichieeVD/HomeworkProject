b1269b0ef7ee4b7e644cb6103489a251a2bfd724
rvd <nseries_nokia@list.ru>
2026-02-24 18:14:26 +0400

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
-        int age = 19;
+        int age = 12;
 
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
         } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+        // Задача 2
+        int temperature = 55;
+
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
     }
 }
