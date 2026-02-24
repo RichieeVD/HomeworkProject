62d4618715350a3499bf7a85c655bfba9470605a
rvd <nseries_nokia@list.ru>
2026-02-24 18:15:24 +0400

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
-        int age = 12;
+        int age = 19;
 
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
@@ -10,12 +10,21 @@ public class Main {
         }
 
         // Задача 2
-        int temperature = 55;
+        int temperature = 4;
 
         if (temperature < 5) {
             System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
         } else {
             System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+
+        // Задача 3
+        int speed = 65; // Можешь менять это число для проверки
+
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
         }
     }
 }
